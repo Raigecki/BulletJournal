a73224a63b57a1babcba855f9ae0d0524df4bfdc
unknown <eewjong@uccsd.edu>
2020-06-24 14:59:24 -0700

    added mock database1 containing bullets

---
+++ bullet-journal/src/data/MockData1.js
@@ -0,0 +1,197 @@
+export const mockData = () => { 
+    return {
+        week: ['07/06/2020', '07/12/2020'],
+        days: [
+            //columns
+            {
+                day: 'Monday',
+                date: '07/06/2020',
+                sections: [
+                    //row
+                    {
+                        category: 'Work',
+                        bullets: [
+                            //points
+                            {
+                                text: 'Work on Bullet Journal'
+                            },
+                            {
+                                text: '3 easy coding problems'
+                            }
+                        ]
+                    },
+                    {
+                        category: 'Recreational',
+                        bullets: [
+                            {
+                                text: 'Stream on Twitch'
+                            },
+                            {
+                                text: 'Practice piano'
+                            } 
+                        ]
+                    }
+                ]
+            },
+            {
+                day: 'Tuesday',
+                date: '07/07/2020',
+                sections: [
+                    //row
+                    {
+                        category: 'Work',
+                        bullets: [
+                            //points
+                            {
+                                text: 'Work on Bullet Journal'
+                            },
+                            {
+                                text: '1 hard coding problems'
+                            }
+                        ]
+                    },
+                    {
+                        category: 'Recreational',
+                        bullets: [
+                            {
+                                text: 'Call bank'
+                            },
+                            {
+                                text: 'Practice guitar'
+                            } 
+                        ]
+                    }
+                ]
+            },
+            {
+                day: 'Wednesday',
+                date: '07/08/2020',
+                sections: [
+                    //row
+                    {
+                        category: 'Work',
+                        bullets: [
+                            //points
+                            {
+                                text: '2 medium coding problems'
+                            }
+                        ]
+                    },
+                    {
+                        category: 'Recreational',
+                        bullets: [
+                            {
+                                text: 'Level up in Guild Wars 2'
+                            },
+                            {
+                                text: 'Practice singing'
+                            } 
+                        ]
+                    }
+                ]
+            },
+            {
+                day: 'Thursday',
+                date: '07/09/2020',
+                sections: [
+                    //row
+                    {
+                        category: 'Work',
+                        bullets: [
+                            //points
+                            {
+                                text: 'Work on Bullet Journal'
+                            },
+                            {
+                                text: '3 easy coding problems'
+                            },
+                            {
+                                text: 'Research mental health'
+                            }
+                        ]
+                    },
+                    {
+                        category: 'Recreational',
+                        bullets: [
+                            {
+                                text: 'Talk to friend'
+                            },
+                            {
+                                text: '50 pushups'
+                            } 
+                        ]
+                    }
+                ]
+            },
+            {
+                day: 'Friday',
+                date: '07/10/2020',
+                sections: [
+                    //row
+                    {
+                        category: 'Work',
+                        bullets: [
+                            //points
+                            {
+                                text: '2 medium coding problems'
+                            }
+                        ]
+                    },
+                    {
+                        category: 'Recreational',
+                        bullets: [
+                            {
+                                text: 'Cook food'
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                day: 'Saturday',
+                date: '07/11/2020',
+                sections: [
+                    //row         
+                    {
+                        category: 'Recreational',
+                        bullets: [
+                            {
+                                text: 'Go biking'
+                            },
+                            {
+                                text: 'Make arrows'
+                            } 
+                        ]
+                    }
+                ]
+            },
+            {
+                day: 'Sunday',
+                date: '07/12/2020',
+                sections: [
+                    //row
+                    {
+                        category: 'Church',
+                        bullets: [
+                            //points
+                            {
+                                text: 'Get lunch with friend'
+                            },
+                            {
+                                text: 'Talk to pastor'
+                            }
+                        ]
+                    },
+                    {
+                        category: 'Recreational',
+                        bullets: [
+                            {
+                                text: 'Get new shoes'
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+}
