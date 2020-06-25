637ea22c4e9a4f09e22d6d8dfe1339430c4d2384
unknown <eewjong@uccsd.edu>
2020-06-24 20:10:09 -0700

    Created new Data Scheme

---
+++ bullet-journal/src/data/MockData2.js
@@ -0,0 +1,137 @@
+export const mockData = () => { 
+    return {
+        week: ['07/06/2020', '07/12/2020'],
+        category: ['Work', 'Casual'],
+        bullets: [
+
+            //////////////////Monday//////////////////
+            {
+                date: '07/06/2020',
+                category: 'Work', 
+                text: 'Work on Bullet Journal'
+            },
+            {
+                date: '07/06/2020',
+                category: 'Work',
+                text: '3 easy coding problems'
+            },
+            {
+                date: '07/06/2020',
+                category: 'Casual',
+                text: 'Stream on Twitch'
+            },
+            {
+                date: '07/06/2020',
+                category: 'Casual',
+                text: 'Practice piano'
+            },
+
+            /////////////////Tuesday//////////////////            
+            {
+                date: '07/07/2020',
+                category: 'Work', 
+                text: 'Work on Bullet Journal'
+            },
+            {
+                date: '07/07/2020',
+                category: 'Work',
+                text: '1 hard coding problem'
+            },
+            {
+                date: '07/07/2020',
+                category: 'Casual',
+                text: 'Call bank'
+            },
+            {
+                date: '07/07/2020',
+                category: 'Casual',
+                text: 'Practice guitar'
+            },
+            
+            /////////////////Wednesday////////////////            
+            {
+                date: '07/08/2020',
+                category: 'Work',
+                text: '2 medium coding problems'
+            },
+            {
+                date: '07/08/2020',
+                category: 'Casual',
+                text: 'Level up in Guild Wars 2'
+            },
+            {
+                date: '07/08/2020',
+                category: 'Casual',
+                text: 'Practice singing'
+            },
+            
+            /////////////////Thursday/////////////////            
+            {
+                date: '07/09/2020',
+                category: 'Work', 
+                text: 'Work on Bullet Journal'
+            },
+            {
+                date: '07/09/2020',
+                category: 'Work',
+                text: '3 easy coding problems'
+            },
+            {
+                date: '07/09/2020',
+                category: 'Work',
+                text: 'Research mental health'
+            },
+            {
+                date: '07/09/2020',
+                category: 'Casual',
+                text: 'Talk to friend'
+            },
+            {
+                date: '07/09/2020',
+                category: 'Casual',
+                text: '50 pushups'
+            },
+
+             /////////////////Friday//////////////////            
+             {
+                date: '07/10/2020',
+                category: 'Work',
+                text: '2 medium coding problems'
+            },
+            {
+                date: '07/10/2020',
+                category: 'Casual',
+                text: 'Cook food'
+            },
+
+            /////////////////Saturday//////////////////            
+            {
+                date: '07/11/2020',
+                category: 'Casual',
+                text: 'Go biking'
+            },
+            {
+                date: '07/11/2020',
+                category: 'Casual',
+                text: 'Make arrows'
+            },
+
+            //////////////////Sunday///////////////////            
+            {
+                date: '07/12/2020',
+                category: 'Church',
+                text: 'Talk to pastor'
+            },
+            {
+                date: '07/12/2020',
+                category: 'Church',
+                text: 'Get lunch with friend'
+            },
+            {
+                date: '07/12/2020',
+                category: 'Casual',
+                text: 'Get new shoes'
+            },
+        ]
+    }
+}
