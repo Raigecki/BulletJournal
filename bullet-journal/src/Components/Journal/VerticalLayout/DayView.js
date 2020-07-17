803e0bd99021864b9a49b67e2586b99216f279ca
unknown <eewjong@uccsd.edu>
2020-07-16 17:06:31 -0700

    Implemented Single List Drag and Drop

--- bullet-journal/src/Components/Journal/VerticalLayout/DayView.js
+++ bullet-journal/src/Components/Journal/VerticalLayout/DayView.js
@@ -1,6 +1,8 @@
-import React, {Fragment, useReducer} from 'react'
+import React, {Fragment, useReducer, useEffect, useState} from 'react'
 import {v4 as uuid} from 'uuid'
 import Bullet from './Bullet';
+import Container from 'react-bootstrap/Container';
+import DragNode from './DragNode';
 
 export const BulletsContext = React.createContext();
 export const TestContext = React.createContext();
@@ -42,14 +44,8 @@ const initialState = {
     bullets: mockBullets
 }
 
-export const testFunction = (arg) => {
-    console.log(arg);
-}
-
 const bulletsReducer = (state, action) => {
-    
-    //const prevState = action.prevState;
-    
+        
     switch(action.type) {
         case 'add' :
             const b = {
@@ -67,7 +63,7 @@ const bulletsReducer = (state, action) => {
             console.log(action.bullet)
             let i = 0; 
             for (; i < state.bullets.length; i++) 
-                if (state.bullets[i].id == action.bullet.id) break;
+                if (state.bullets[i].id === action.bullet.id) break;
             
             console.log(i)
             const newBullets = JSON.parse(JSON.stringify(state.bullets))
@@ -77,35 +73,84 @@ const bulletsReducer = (state, action) => {
 
             }
 
+        case 'drag':
+            const curr = {...state.bullets[action.dragIndex]};
+            const copy = [...state.bullets]
+            copy.splice(action.dragIndex, 1)
+            copy.splice(action.enterIndex, 0, curr);
+            return {...state, bullets: copy}
+                            
         case 'remove' :
             console.log('Action', action)
             console.log('state', state)
             return {...state, 
-                bullets: state.bullets.filter(e => e.id != action.id)
+                bullets: state.bullets.filter(e => e.id !== action.id)
             }
+        default:
+            return state
                 
     }
 }
 
+
 function DayView() {
 
     const [state, dispatchBullets] = useReducer(bulletsReducer, initialState);
-    console.log(state)
+    const [dragging, setDragging] = useState({})
+    
+    const handleDragStart = (e, itemIndex, dragging) => {
+        dragging.dragItem = itemIndex;
+        dragging.dragElem = e.target;
+        toggleDragStyle(e, dragging)
+        console.log('Begin dragging', dragging)
+    }
+
+    const handleDragEnd = (e, dragging) => {
+        dragging = null;
+        toggleDragStyle(e, dragging)
+        console.log('End drag', dragging)
+    }
+
+    const handleDragEnter = (e, i) => {
+        if (i !== dragging.dragItem) {
+            dispatchBullets({
+                type: 'drag', 
+                enterIndex: i, 
+                dragIndex: dragging.dragItem
+            })
+            dragging.dragItem = i;
+        }
+    }
+
+    const toggleDragStyle = (e, dragging) => {
+        console.log('Toggle style')
+        
+        if (dragging)
+            e.target.style.backgroundColor = 'lightblue'
+        else 
+            e.target.style.backgroundColor = 'transparent'
+    }
 
     return (
-        <Fragment>
-
+        <Fragment>          
             {state.day.name} <br/>
             {state.day.date}
-
-            {state.bullets.map(b => {
-                return (
-                    <Bullet key={b.id} bullet={b} />
-                )
-            })}
+            {state.bullets.map((b, i) => (
+                <div
+                    key={b.id}
+                    draggable
+                    onDragStart={e => handleDragStart(e, i, dragging)}
+                    onDragEnd={e => handleDragEnd(e, dragging)}
+                    onDragEnter={e => {handleDragEnter(e, i)}}
+                >
+                    <Bullet  bullet={b} />
+                </div>
+                
+            ))}
 
         </Fragment>
     )
 }
+
 
 export default DayView
