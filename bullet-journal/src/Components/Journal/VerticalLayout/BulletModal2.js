8aabc8f4b586b029aa2a96b9ec851c4c94debf57
unknown <eewjong@uccsd.edu>
2020-09-04 17:48:17 -0700

    Changed Form styles

--- bullet-journal/src/Components/Journal/VerticalLayout/BulletModal2.js
+++ bullet-journal/src/Components/Journal/VerticalLayout/BulletModal2.js
@@ -1,5 +1,5 @@
 import React, {useState, useContext} from 'react'
-import Modal from 'react-bootstrap/Modal'
+import {Modal, Form, Button} from 'react-bootstrap'
 import Select from 'react-select'
 import {DayBulletsContext} from './DayView'
 import {WeekBulletsContext} from './WeekView'
@@ -11,7 +11,7 @@ function BulletModal2(props) {
     const currBulletsContext = props.view === 'day' ? 
     DayBulletsContext : WeekBulletsContext
     const bulletsContext = useContext(currBulletsContext)
-    console.log('Categories', props.categories)
+
     return (
         <Modal
             {...props}
@@ -23,29 +23,30 @@ function BulletModal2(props) {
                 <Modal.Title>Add Bullet</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <select
-                    defaultValue={'DEFAULT'}
-                    onChange={e => setForm({...form, category: e.target.value})}
-                >
-                    <option value="DEFAULT" disabled>Select Category</option>
-                    {props.categories.map(c => 
-                        <option key={c} value={c}>{c}</option>
-                    )}
-                </select> <br />
-                <textarea type="text" placeholder="Description" 
-                    onChange={e => setForm({...form, text: e.target.value})}
-                />
+                <Form>
+                    <Form.Control as="select">
+                        <option value="DEFAULT" disabled>Select Category</option>
+                        {props.categories.map(c => 
+                            <option key={c} value={c}>{c}</option>
+                        )}
+                    </Form.Control>
+                    <Form.Control as="textarea" placeholder="Description" />
+                    <p></p>
+                </Form>
             </Modal.Body>
             <Modal.Footer>
-                <button onClick={() => {
-                    bulletsContext.dispatch({
-                        type: 'add',
-                        date: '07/01/2020',
-                        category: form.category,
-                        text: form.text
-                    });
-                    props.onHide();
-                }}>Save</button>
+                <Button
+                    variant="primary"
+                    onClick={() => {
+                        bulletsContext.dispatch({
+                            type: 'add',
+                            date: '07/01/2020',
+                            category: form.category,
+                            text: form.text
+                        });
+                        props.onHide();
+                    }}
+                >Save</Button>
             </Modal.Footer>
         </Modal>
     )
